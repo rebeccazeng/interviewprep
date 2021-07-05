f7d7d2c8ad1780ccfbf87acf2d6d9c6bb769867e
Rebecca Zeng <rebeccazeng13@gmail.com>
2021-07-05 13:53:28 -0700

    triplets updated

---
+++ two_pointers/triplet_sum_close_to_target.py
@@ -0,0 +1,69 @@
+'''
+Triplet Sum Close to Target (medium)
+Problem Statement 
+Given an array of unsorted numbers and a target number, 
+find a triplet in the array whose sum is as close to the target number as possible, return the sum of the triplet. 
+If there are more than one such triplet, return the sum of the triplet with the smallest sum.
+Example 1:
+Input: [-2, 0, 1, 2], target=2
+Output: 1
+Explanation: The triplet [-2, 1, 2] has the closest sum to the target.
+Example 2:
+Input: [-3, -1, 1, 2], target=1
+Output: 0
+Explanation: The triplet [-3, 1, 2] has the closest sum to the target.
+Example 3:
+Input: [1, 0, 1, 1], target=100
+Output: 3
+Explanation: The triplet [1, 1, 1] has the closest sum to the target.
+'''
+# my answer
+def triplet_sum_closest_to_target(arr, target):
+	closest_sum = target 
+	closest_triplet = []
+	arr.sort()
+
+	def search_pairs(array, current_number, left):
+		right=len(array)-1
+		while left < right:
+			current_sum = current_number + array[left] + array[right]
+			if current_sum > closest_sum:
+				closest_sum = current_sum
+				closest_triplet = [current_number, array[left], array[right]]
+			elif current_sum > target:
+				right -= 1
+			else:
+				left += 1
+
+	for i in range(len(arr)-1):
+		search_pairs(arr, arr[i], i+1, closest_triplet, closest_sum)
+	return closest_sum
+
+# answer
+
+import math
+def triplet_sum_close_to_target(arr, target_sum):
+  arr.sort()
+  smallest_difference = math.inf
+  for i in range(len(arr)-2):
+    left = i + 1
+    right = len(arr) - 1
+    while (left < right):
+      target_diff = target_sum - arr[i] - arr[left] - arr[right]
+      if target_diff == 0:  # we've found a triplet with an exact sum
+        return target_sum - target_diff  # return sum of all the numbers
+
+      # the second part of the following 'if' is to handle the smallest sum when we have more than one solution
+      if abs(target_diff) < abs(smallest_difference) or (abs(target_diff) == abs(smallest_difference) and target_diff > smallest_difference):
+        smallest_difference = target_diff  # save the closest and the biggest difference
+
+      if target_diff > 0:
+        left += 1  # we need a triplet with a bigger sum
+      else:
+        right -= 1  # we need a triplet with a smaller sum
+
+  return target_sum - smallest_difference
+
+
+
+
