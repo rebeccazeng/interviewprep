ca8e8769d3f832a3f0291f3b366b25e220b414ee
Rebecca Zeng <rebeccazeng13@gmail.com>
2021-07-27 20:27:26 -0700

    Time: 60 ms (91.85%), Space: 15 MB (60.36%) - LeetHub

--- maximum-subarray/maximum-subarray.py
+++ maximum-subarray/maximum-subarray.py
@@ -1,6 +1,43 @@
-import math
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
+        if (len(nums) == 0):
+            return 0
+        maxSum = nums[0]
+        currentSum = nums[0]
+        # [-2,1,-3,4,-1,2,1,-5,4]
+        for i in range(1, len(nums)):
+            n = nums[i]
+            currentSum = max(currentSum + n, n)
+            # is this number + past currentSum better or worse than just having the number on its own?
+            maxSum = max(maxSum, currentSum)
+            # is the currentSum better than the past maxSum that we got?
+        return maxSum
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         if (len(nums) == 0):
             return 0
         maxSum = nums[0]
